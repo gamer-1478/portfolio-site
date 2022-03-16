f64c6e68d6f54ce4cd5c1ada70ad07f43f6785ed
Aayush Garg <aayushgarg2006@gmail.com>
2022-03-16 23:36:44 +0530

    update ReactApp

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function AboutMe() {
   return (
-      <div style={{ height: '100vh', backgroundColor:'#CABBE9'}}>AboutMe</div>
+      <div id='aboutme' style={{ height: '100vh', backgroundColor:'#CABBE9'}}>AboutMe</div>
   )
 }
 
