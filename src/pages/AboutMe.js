09eeac852c4cfb7a0ef1c12fb980ebb2f986cf4c
Aayush Garg <aayushgarg2006@gmail.com>
2022-03-17 11:50:13 +0530

    update

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,8 +1,45 @@
-import React from 'react'
+import React from 'react';
+import './aboutMe.css';
 
 function AboutMe() {
   return (
-      <div id='aboutme' style={{ height: '100vh', backgroundColor:'#CABBE9'}}>AboutMe</div>
+    <div id='aboutme' className='Aboutme-root'>
+      <div className='ohk-root' style={{ width: '80vw', display: 'flex' }}>
+        <div className='aboutme-container'>
+          <h1 className='aboutme-header'>About me</h1>
+          <p className='aboutme-connect-dots' style={{ fontSize: '1.3rem', color: '#2B1D6D' }}>Connecting the dots</p>
+          <div className='line-effect'>
+            <span className='line-about'></span>
+            <span className='circle-about'></span>
+          </div>
+          <h2 style={{ padding: '1vw 0vw 1vw 0vw', fontSize: '2.5rem', fontWeight: '300' }}>Hi there!</h2>
+          <div className='about-main-content'>
+            <p>I am Anirudh Chaturvedi, a high school senior and the Vice Head Boy
+              at Amity International school sector-46.</p><br></br>
+
+            <p>I've always been very passionate to explore different fields and take
+              up various co-curricular activities other than academics. I love doing
+              video editing, especially motion graphics and graphic designing as a
+              passion. I am also a core-member of Tech Syndicate, the technology
+              organization and the secretary of Cyber Congress of my school.</p><br></br>
+
+            <p>Being a fitness enthusiast I actively take up long distance cycling and
+              running. Check out my strava profile for more and follow:)</p><br></br>
+
+            <p>Music has always been an inseparable part of my life, I am a guitarist
+              and currently pursuing music studies from Trinity college London.
+              Fun fact I'm also a melomaniac, my taste in music ranges from
+              pop rock to blues to jazz to country music. I am a huge fan of John Mayer!</p>
+            <br></br>
+            <p>I love to travel, watch videos and read about armed forces, write poems,
+              watch movies and web-series and talk to people! </p>
+          </div>
+        </div>
+        <div style={{ display: 'flex', alignItems: 'center', justifyContent:'center', flexDirection: 'column' }} className='aboutme-img-carasol'>
+          <img style={{marginTop:'20px', height: '600px' }} alt='img' src='https://cdn.discordapp.com/attachments/840934970548813854/953882671245840414/Group_10.png' />
+        </div>
+      </div>
+    </div >
   )
 }
 
