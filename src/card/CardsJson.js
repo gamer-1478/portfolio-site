6bc007d1559c4cd77a0a3f528f49b3e3bb757369
Aayush Garg <aayushgarg2006@gmail.com>
2022-03-17 19:17:30 +0530

    Update CardsJson

--- src/card/CardsJson.js
+++ src/card/CardsJson.js
@@ -1,7 +1,7 @@
 export default function CardJSON() {
     return JSON.parse(JSON.stringify([
         {
-            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/953972124073218119/The_Sakaram_RSS_motors_Promo.gif',
+            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/954008717953736755/The_Sakaram_RSS_motors_Promo.gif',
             title: 'The Sakaram',
             text: 'Promotional video for The Sakaram - RSS motors',
             label: 'Video Editing',
@@ -9,7 +9,7 @@ export default function CardJSON() {
             check_on: 'YouTube'
         },
         {
-            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/953974234361774120/Robotronics_21_Opening_Video_Tech_Syndicate.gif',
+            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/954008775499583498/Robotronics_21_Opening_Video_Tech_Syndicate.gif',
             text: "Official teaser for 8th edition of Tech syndicate’s flagship event",
             title: 'Robotronics Teaser (2021)',
             label: 'Video Editing',
@@ -17,7 +17,7 @@ export default function CardJSON() {
             check_on: 'YouTube'
         },
         {
-            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/953968136867155978/Cbse2_0_Promo.gif',
+            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/954008760865677322/Cbse2_0_Promo.gif',
             text: 'Promotional video for CBSE revolutionized(concept)',
             title: 'CBSE 2.0 (2021)',
             label: 'Video Editing',
@@ -25,7 +25,7 @@ export default function CardJSON() {
             check_on: 'YouTube'
         },
         {
-            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/953967855299334194/Obliviousness_A_short_film.gif',
+            img_src: '/oblivious.gif',
             text: 'A man finds an abandoned pair of glasses...',
             title: 'Obliviousness - film (2021)',
             label: 'Video Editing',
@@ -33,7 +33,7 @@ export default function CardJSON() {
             check_on: 'YouTube'
         },
         {
-            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/953967808289587230/metaworks.gif',
+            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/954008747318067280/metaworks.gif',
             text: 'Promotional video for Metaworks.',
             title:'Metaworks (2022)',
             label: 'Video Editing',
@@ -41,7 +41,7 @@ export default function CardJSON() {
             check_on:'YouTube'
         },
         {
-            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/953967808054689843/TEDx.gif',
+            img_src: 'https://cdn.discordapp.com/attachments/840934970548813854/954008725193129984/TEDx.gif',
             text: 'Official making video for TEDxYouth2021@AIS46',
             title: 'TEDx@AIS46 Video (2021)',
             label: 'Video Editing',
